6e0292bb70c0b5400692c6cfb8d96f7cc19b9433
JCPrime01 <vanzokiller@gmail.com>
2026-05-13 22:07:09 -0300

    update shareCode 6BW3HUUM0RJ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=X5R2XEINFRL&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=6BW3HUUM0RJ&afp=tapi100' });
     res.end();
   }
