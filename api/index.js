254a67fd1a290d0a28ea1bb5b8ddeb3f79aed1de
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-22 19:58:16 -0300

    chore: update shareCode to RA4C3KNXG2Y

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=BXCGWK8WMOI&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=RA4C3KNXG2Y&afp=tapi100' });
     res.end();
   }
