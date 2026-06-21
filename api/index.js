62d726ff8dc4dd8699658d304a06f8e7459d148b
JCPrime01 <vanzokiller@gmail.com>
2026-06-21 11:47:52 -0300

    chore: shareCode CICEHYO0CVA -> ZB48YUK42K7 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=CICEHYO0CVA'
+    const shareCode = '&shareCode=ZB48YUK42K7'
     
     const afp = '&afp2=tp'
     
