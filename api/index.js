d7544cf8197f79a32d11214e3fb9969580edfddc
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 22:13:34 -0300

    chore: shareCode YOOVKBFINMQ -> F290U89J3J8 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=YOOVKBFINMQ'
+    const shareCode = '&shareCode=F290U89J3J8'
     
     const afp = '&afp2=tp'
     
