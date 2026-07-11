774cd83f114f166df06830726037d1a41378c09c
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 13:26:50 -0300

    chore: shareCode 6L6VIGQL3WI -> D2X4Q502MGP (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=6L6VIGQL3WI'
+    const shareCode = '&shareCode=D2X4Q502MGP'
     
     const afp = '&afp2=tp'
     
