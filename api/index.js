f575b066510b98f686e117e6ceceffae996633f4
JCPrime01 <vanzokiller@gmail.com>
2026-08-07 20:25:33 -0300

    chore: shareCode OSH985O4PO3 -> JNWI23CT21K (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180'
 
-    const shareCode = '&shareCode=OSH985O4PO3'
+    const shareCode = '&shareCode=JNWI23CT21K'
     
     const afp = '&afp2=tp'
     
